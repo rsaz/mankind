dafdbdfcab94673ff23613470033514c67d8fe71
Delacroz <andremenezes970@gmail.com>
2020-10-25 23:51:17 -0300

    Update DayNight.cs

--- Assets/_Game/Scripts/DayNight.cs
+++ Assets/_Game/Scripts/DayNight.cs
@@ -5,39 +5,58 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class DayNight : MonoBehaviour
 {
     [SerializeField] private Light2D sun;
-    [SerializeField] private float intensityFactor = 0.01f;
+    
+    [SerializeField] private float dayDurationInMinutes= 1;
+    [SerializeField] private float nightDurationInMinutes = 0.5f;
+
+    [SerializeField] private float totalDarknnesStayTime = 1f;
+    [SerializeField] private float fullBrightnessStayTime = 1f;
+
+    [Range(0, 1)]
+    [SerializeField] private float maxDayLightIntensity = 1.0f;
+    [Range(0, 1)]
+    [SerializeField] private float maxNightLightIntensity = 0.5f;
+
+    private float dayIntensityFactor;
+    private float nightIntensityFactor;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        dayIntensityFactor = (maxDayLightIntensity - maxNightLightIntensity) / (dayDurationInMinutes * 6000);
+        nightIntensityFactor = (maxDayLightIntensity - maxNightLightIntensity) / (nightDurationInMinutes * 6000);
         StartCoroutine(Sunset());
+        
     }
 
     IEnumerator Sunset()
     {
         // to define a day and night cycle time
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(fullBrightnessStayTime * 60);
 
-        while (sun.intensity > 0.5f)
+        while (sun.intensity > maxNightLightIntensity)
         {
-            sun.intensity -= intensityFactor;
-            yield return new WaitForSeconds(intensityFactor);
+            
+            sun.intensity -= dayIntensityFactor;
+            yield return new WaitForSeconds(0.01f);
         }
-       
+        
         StartCoroutine(Sunrise());
-        yield return new WaitForSeconds(5f);
-        StartCoroutine(Sunset());
     }
 
     IEnumerator Sunrise()
     {
+        yield return new WaitForSeconds(totalDarknnesStayTime * 60);
         while (sun.intensity < 1f)
         {
-            sun.intensity += intensityFactor;
-            yield return new WaitForSeconds(intensityFactor);
+            sun.intensity += nightIntensityFactor;
+            yield return new WaitForSecondsRealtime(0.01f);
         }
         StartCoroutine(Sunset());
-        yield return new WaitForSeconds(5f);
-        StartCoroutine(Sunrise());
+        
+
     }
 }
