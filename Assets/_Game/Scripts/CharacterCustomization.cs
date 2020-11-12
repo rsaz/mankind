18aee3c25d7a4cc9a4ade5ce9d22de64d11dfe76
Suziane Cundiff <svlc@icomp.ufam.edu.br>
2020-11-11 21:08:19 -0400

    Character Customization (#22)
    * CharacterCustomization
    
    Any part of the body can be changed, I forgot the weapons but I will put them as soon as possible. I started to implement a function that made the pairs part change for the same piece but it's not completed yet
    
    * Update PhotonServerSettings.asset
    
    Changed for the original settings

--- Assets/_Game/Scripts/CharacterCustomization.cs
+++ Assets/_Game/Scripts/CharacterCustomization.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿using System.Security.AccessControl;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 using UnityEngine;
 
 public class CharacterCustomization : MonoBehaviour
 {
+    #region Properties
+
     [Header("Body part to be changed")]
     public SpriteRenderer part;
 
     [Header("Sprites to select")]
     public List<Sprite> options = new List<Sprite>();
 
-    private int currentOption = 0;
+    //SpriteRenderer currentPart = part;
+    public int currentOption = 0;
+
+    #endregion
+
+    #region Methods
 
     public void NextOption()
     {
@@ -32,4 +42,10 @@ public class CharacterCustomization : MonoBehaviour
         }
         part.sprite = options[currentOption];
     }
+    public void RandomCustomization(){    
+        currentOption = Random.Range(0, options.Count - 1);
+        part.sprite = options[currentOption];
+    }
+
+    #endregion
 }
---
+++ Assets/_Game/Scripts/CharacterCustomizationManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.AccessControl;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CharacterCustomizationManager : MonoBehaviour
+{
+    public List<CharacterCustomization> characterCustomization = new List<CharacterCustomization>();
+   
+    public void RandomOption()
+    {
+        foreach (CharacterCustomization option in characterCustomization)
+        {
+            option.RandomCustomization();
+        }
+    }
+
+}
