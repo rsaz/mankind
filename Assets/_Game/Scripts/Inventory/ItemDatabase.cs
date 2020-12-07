f086764a239d269cb93f1e74f9ea1c76aa8c0234
Thyago Quintas <thyagoquintas@hotmail.com>
2020-12-06 21:32:11 -0300

    Implementation of inventory code and UI example (#39)

---
+++ Assets/_Game/Scripts/Inventory/ItemDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    private void Awake()
+    {
+        BuildDatabase();
+    }
+
+    public Item GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    public Item GetItem(string itemName)
+    {
+        return items.Find(item => item.title == itemName);
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item>() {
+                new Item(0, "Diamond Sword", "A sword made with diamond.",
+                    new Dictionary<string, int>
+                    {
+                        {"Power", 15},
+                        {"Defence", 10},
+                        {"Value", 600 }
+                    }),
+                new Item(1, "Diamond Ore", "A beautiful diamond.",
+                    new Dictionary<string, int>
+                    {
+                        {"Value", 444}
+                    }),
+                new Item(2, "Silver Pick", "A very nice pick.",
+                    new Dictionary<string, int>
+                    {
+                        {"Power", 15},
+                        {"Mining", 333},
+                        {"Value", 50 }
+                    }),
+            };
+    }
+}
